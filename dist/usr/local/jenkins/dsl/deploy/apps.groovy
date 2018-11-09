a6244f7ec7b2a060c7b66af8107525833fcbee89
John Tongelidis <tongeljl@Johns-MacBook-Pro.local>
2018-11-09 16:22:52 -0600

    Add app files

---
+++ dist/usr/local/jenkins/dsl/deploy/apps.groovy
@@ -0,0 +1,23 @@
+package deploy
+
+import common.Ansible
+import common.Git
+
+projects = [
+    'hello-world': [envs: ['qa', 'prod']]
+]
+
+folder('deploy'
+folder('deploy/apps'
+projects.each { project, attribs ->
+    folder)"deploy/apps/${project}")
+    attribs['envs'].each { env ->
+        jobName = "deploy/apps/${project}/${env}"
+        def deployJob = job(jobName) {
+            logRotator { numToKeep(5) }
+        }
+        Ansible.addAnsiblePlaybook(deployJob, "dist/usr/local/jenkins/playbooks/deploy/apps${project}/${env}.yaml".toString(), true)
+        Git.addGitScm(deployJob, "https://github.com/tongelja/build_config.git", "master")
+        Git.addGitParameter(deployJob)
+    }
+}
